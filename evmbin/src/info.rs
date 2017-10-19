e6283149446dfc5d406dcc4fedfb859bb1afba86
cdetrio <cdetrio@gmail.com>
2017-10-19 16:41:19 -0400

    standardized tracing

--- evmbin/src/info.rs
+++ evmbin/src/info.rs
@@ -113,7 +113,8 @@ pub fn run_transaction<T: Informant>(
 					post_root,
 				))), None)
 			},
-			TransactResult::Ok { gas_left, output, vm_trace, .. } => {
+			TransactResult::Ok { state_root, gas_left, output, vm_trace, .. } => {
+				println!("{{\"stateRoot\": \"{:?}\"}}", state_root);
 				(Ok((gas_left, output)), vm_trace)
 			},
 			TransactResult::Err { error, .. } => {
